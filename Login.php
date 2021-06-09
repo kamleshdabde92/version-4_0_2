f20c7379568f25c94b67e1dc111e84cf2c53719a
kamleshdabde92 <kamleshdabde@gmail.com>
2021-06-09 22:36:55 +0530

    New file add

---
+++ Login.php
@@ -0,0 +1,349 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+        $this->load->database();
+        $this->load->library('session');
+        /*cache control*/
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $this->output->set_header('Pragma: no-cache');
+    }
+
+    public function index() {
+        if ($this->session->userdata('admin_login')) {
+            redirect(site_url('admin'), 'refresh');
+        }elseif ($this->session->userdata('user_login')) {
+            redirect(site_url('user'), 'refresh');
+        }else {
+            redirect(site_url('home/login'), 'refresh');
+        }
+    }
+
+    public function validate_login($from = "") {
+        $mobile_no = $this->input->post('mobile_no');
+        $password = $this->input->post('password');
+        $credential = array('mobile_no' => $mobile_no, 'password' => sha1($password), 'status' => 1);
+        // Checking login credential for admin
+        $query = $this->db->get_where('users', $credential);
+
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+            $this->session->set_userdata('user_id', $row->id);
+            $this->session->set_userdata('role_id', $row->role_id);
+            $this->session->set_userdata('role', get_user_role('user_role', $row->id));
+            $this->session->set_userdata('name', $row->first_name.' '.$row->last_name);
+            $this->session->set_userdata('is_instructor', $row->is_instructor);
+            $this->session->set_flashdata('flash_message', get_phrase('welcome').' '.$row->first_name.' '.$row->last_name);
+            if ($row->role_id == 1) {
+                $this->session->set_userdata('admin_login', '1');
+                redirect(site_url('admin/dashboard'), 'refresh');
+            }else if($row->role_id == 2){
+                $this->session->set_userdata('user_login', '1');
+                redirect(site_url('home'), 'refresh');
+            }
+        }else {
+            $this->session->set_flashdata('error_message',get_phrase('invalid_login_credentials'));
+            redirect(site_url('home/login'), 'refresh');
+        }
+    }
+
+    public function validate_login1($from = "") {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $credential = array('email' => $email, 'password' => sha1($password), 'status' => 1);
+
+        // Checking login credential for admin
+        $query = $this->db->get_where('users', $credential);
+
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
+            $this->session->set_userdata('user_id', $row->id);
+            $this->session->set_userdata('role_id', $row->role_id);
+            $this->session->set_userdata('role', get_user_role('user_role', $row->id));
+            $this->session->set_userdata('name', $row->first_name.' '.$row->last_name);
+            $this->session->set_userdata('is_instructor', $row->is_instructor);
+            $this->session->set_flashdata('flash_message', get_phrase('welcome').' '.$row->first_name.' '.$row->last_name);
+            if ($row->role_id == 1) {
+                $this->session->set_userdata('admin_login', '1');
+                redirect(site_url('admin/dashboard'), 'refresh');
+            }else if($row->role_id == 2){
+                $this->session->set_userdata('user_login', '1');
+                redirect(site_url('home'), 'refresh');
+            }
+        }else {
+            $this->session->set_flashdata('error_message',get_phrase('invalid_login_credentials'));
+            redirect(site_url('home/login'), 'refresh');
+        }
+    }
+
+    public function register() {
+        $data['first_name'] = html_escape($this->input->post('first_name'));
+        $data['last_name']  = html_escape($this->input->post('last_name'));
+        $data['email']  = html_escape($this->input->post('email'));
+        $data['password']  = sha1($this->input->post('password'));
+        $data['pincode']  = $this->input->post('pincode');
+        $data['state']  = html_escape($this->input->post('state'));
+        $data['district']  = html_escape($this->input->post('district'));
+// print_r($data);die;
+        $verification_code =  md5(rand(100000000, 200000000));
+        $data['verification_code'] = $verification_code;
+
+        if (get_settings('student_email_verification') == 'enable') {
+            $data['status'] = 0;
+        }else {
+            $data['status'] = 1;
+        }
+
+        $data['wishlist'] = json_encode(array());
+        $data['watch_history'] = json_encode(array());
+        $data['date_added'] = strtotime(date("Y-m-d H:i:s"));
+        $social_links = array(
+            'facebook' => "",
+            'twitter'  => "",
+            'linkedin' => ""
+        );
+        $data['social_links'] = json_encode($social_links);
+        $data['role_id']  = 2;
+
+        // Add paypal keys
+        $paypal_info = array();
+        $paypal['production_client_id'] = "";
+        array_push($paypal_info, $paypal);
+        $data['paypal_keys'] = json_encode($paypal_info);
+        // Add Stripe keys
+        $stripe_info = array();
+        $stripe_keys = array(
+            'public_live_key' => "",
+            'secret_live_key' => ""
+        );
+        array_push($stripe_info, $stripe_keys);
+        $data['stripe_keys'] = json_encode($stripe_info);
+
+        $validity = $this->user_model->check_duplication('on_create', $data['email']);
+        if ($validity) {
+            $user_id = $this->user_model->register_user($data);
+            /*$this->session->set_userdata('user_login', '1');
+            $this->session->set_userdata('user_id', $user_id);
+            $this->session->set_userdata('role_id', 2);
+            $this->session->set_userdata('role', get_user_role('user_role', 2));
+            $this->session->set_userdata('name', $data['first_name'].' '.$data['last_name']);*/
+
+            if (get_settings('student_email_verification') == 'enable') {
+                $this->email_model->send_email_verification_mail($data['email'], $verification_code);
+                $this->session->set_flashdata('flash_message', get_phrase('your_registration_has_been_successfully_done').'. '.get_phrase('please_check_your_mail_inbox_to_verify_your_email_address').'.');
+            }else {
+                $this->session->set_flashdata('flash_message', get_phrase('your_registration_has_been_successfully_done'));
+            }
+
+        }else {
+            $this->session->set_flashdata('error_message', get_phrase('email_duplication'));
+        }
+        redirect(site_url('home'), 'refresh');
+    }
+
+
+
+    public function sentotp() {
+        $array = [];
+        $data['last_name'] = html_escape($this->input->post('last_name'));
+        $data['first_name'] = html_escape($this->input->post('first_name'));
+        $data['email'] = html_escape($this->input->post('email'));
+        $data['password'] = html_escape($this->input->post('password'));
+        $data['mobile_no'] = html_escape($this->input->post('mobile_no'));
+        $data['state'] = $this->input->post('state');
+        $data['district'] = $this->input->post('district');
+        $data['pincode'] = html_escape($this->input->post('pincode'));
+        $verification_code =  rand(100000, 200000);
+        
+        // print_r($_POST);die;
+       
+        $validity = $this->user_model->check_duplication_mobile('on_create', $data['mobile_no']);
+        if ($validity) {
+            // $emailvalidity = $this->user_model->check_duplication('on_create', $data['email']);
+                $this->session->set_userdata('last_name', $data['last_name']);
+                $this->session->set_userdata('first_name',$data['first_name']);
+                $this->session->set_userdata('email',$data['email']);
+                $this->session->set_userdata('password',$data['password']);
+                $this->session->set_userdata('mobile_no',$data['mobile_no']);
+                $this->session->set_userdata('state',$data['state']);
+                $this->session->set_userdata('district',$data['district']);
+                $this->session->set_userdata('pincode',$data['pincode']);
+                $this->session->set_userdata('verification_code',$verification_code);
+
+                $msg     =  "Hello user, Your verification OTP ".$verification_code;
+                
+                $this->send_otp($data['mobile_no'], $msg);
+
+                $this->session->set_flashdata('flash_message', "Your otp has send to your mobile number. ". $verification_code.'.');
+                $array['status'] = 'success';
+                $array['message'] = 'Your otp has send to your mobile number.';
+                $array['verification_code'] = $verification_code;
+        }else {
+            $this->session->set_flashdata('error_message', 'Mobile number already exist');
+              $array['status'] = 'error';
+              $array['message'] = 'Mobile number already exist';
+        }
+       echo json_encode($array);
+    }
+
+    public function otpVerify(){
+        $array = [];
+        $data['mobile_otp'] = html_escape($this->input->post('otp'));
+        $arr = $this->session->userdata();
+        // print_r($arr);die;
+        $data['last_name']=    html_escape($arr['last_name']);
+        $data['first_name']=   html_escape($arr['first_name']);
+        $data['email']=        html_escape($arr['email']);
+        $data['password']=     html_escape($arr['password']);
+        $data['mobile_no']=    html_escape($arr['mobile_no']);
+        $data['state']=        html_escape($arr['state']);
+        $data['district']=     html_escape($arr['district']);
+        $data['pincode']=      html_escape($arr['pincode']);
+        $data['verification_code']= html_escape($arr['verification_code']);
+
+        if ($data['mobile_otp'] != $data['verification_code'] ) {
+            $this->session->set_flashdata('error_message', get_phrase('wrong_otp'));
+                  $array['status'] = "error";
+                  $array['message'] =  'Wrong otp';
+        } else{
+
+            $data['password']   = sha1($data['password']);
+            $data['status'] = 1;
+            $data['wishlist'] = json_encode(array());
+            $data['watch_history'] = json_encode(array());
+            $data['date_added'] = strtotime(date("Y-m-d H:i:s"));
+            $social_links = array(
+                'facebook' => "",
+                'twitter'  => "",
+                'linkedin' => ""
+            );
+            $data['social_links'] = json_encode($social_links);
+            $data['role_id']  = 2;
+
+            // Add paypal keys
+            $paypal_info = array();
+            $paypal['production_client_id'] = "";
+            array_push($paypal_info, $paypal);
+            $data['paypal_keys'] = json_encode($paypal_info);
+            // Add Stripe keys
+            $stripe_info = array();
+            $stripe_keys = array(
+                'public_live_key' => "",
+                'secret_live_key' => ""
+            );
+            array_push($stripe_info, $stripe_keys);
+            $data['stripe_keys'] = json_encode($stripe_info);
+            
+            $user_id = $this->user_model->register_user($data);
+            $this->session->set_flashdata('flash_message', get_phrase('your_registration_has_been_successfully_done'));
+            $this->session->unset_userdata('last_name');
+            $this->session->unset_userdata('first_name');
+            $this->session->unset_userdata('email');
+            $this->session->unset_userdata('password');
+            $this->session->unset_userdata('mobile_no');
+            $this->session->unset_userdata('state');
+            $this->session->unset_userdata('district');
+            $this->session->unset_userdata('verification_code');
+            $array['status'] = "success";
+            $array['message'] =  get_phrase('your_registration_has_been_successfully_done');
+        }
+        echo json_encode($array);
+    }
+
+    
+
+    public function logout($from = "") {
+        //destroy sessions of specific userdata. We've done this for not removing the cart session
+        $this->session_destroy();
+        redirect(site_url('home/login'), 'refresh');
+    }
+
+    public function session_destroy() {
+        $this->session->unset_userdata('user_id');
+        $this->session->unset_userdata('role_id');
+        $this->session->unset_userdata('role');
+        $this->session->unset_userdata('name');
+        $this->session->unset_userdata('is_instructor');
+        if ($this->session->userdata('admin_login') == 1) {
+            $this->session->unset_userdata('admin_login');
+        }else {
+            $this->session->unset_userdata('user_login');
+        }
+    }
+
+    function forgot_password($from = "") {
+        $email = $this->input->post('email');
+        //resetting user password here
+        $new_password = substr( md5( rand(100000000,20000000000) ) , 0,7);
+
+        // Checking credential for admin
+        $query = $this->db->get_where('users' , array('email' => $email));
+        if ($query->num_rows() > 0)
+        {
+            $this->db->where('email' , $email);
+            $this->db->update('users' , array('password' => sha1($new_password)));
+            // send new password to user email
+            $this->email_model->password_reset_email($new_password, $email);
+            $this->session->set_flashdata('flash_message', get_phrase('please_check_your_email_for_new_password'));
+            if ($from == 'backend') {
+                redirect(site_url('login'), 'refresh');
+            }else {
+                redirect(site_url('home'), 'refresh');
+            }
+        }else {
+            $this->session->set_flashdata('error_message', get_phrase('password_reset_failed'));
+            if ($from == 'backend') {
+                redirect(site_url('login'), 'refresh');
+            }else {
+                redirect(site_url('home'), 'refresh');
+            }
+        }
+    }
+
+    public function verify_email_address($verification_code = "") {
+        $user_details = $this->db->get_where('users', array('verification_code' => $verification_code));
+        if($user_details->num_rows() == 0) {
+            $this->session->set_flashdata('error_message', get_phrase('email_duplication'));
+        }else {
+            $user_details = $user_details->row_array();
+            $updater = array(
+                'status' => 1
+            );
+            $this->db->where('id', $user_details['id']);
+            $this->db->update('users', $updater);
+            $this->session->set_flashdata('flash_message', get_phrase('congratulations').'!'.get_phrase('your_email_address_has_been_successfully_verified').'.');
+        }
+        redirect(site_url('home'), 'refresh');
+    }
+
+    public function send_otp($mobile_no = "9131970206",$msg="Hello user, Your verificarion OTP"){
+       
+        $auth_key = "d61ad9ea5fa5776ad9f2e4161a43874";
+        
+        $message = $msg;
+
+        $sender = "TBTSIG"; //ex:INVITE
+
+        $mobile_number = $mobile_no;
+      
+        $url = "http://sms.fbipool.com/rest/services/sendSMS/sendGroupSms?AUTH_KEY=".$auth_key."&message=".urlencode($message)."&senderId=".$sender."&routeId=1&mobileNos=".$mobile_number."&smsContentType=english";
+            
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $curl_scraped_page = curl_exec($ch);
+        if (curl_errno($ch)) {
+            $error_msg = curl_error($ch);
+           
+        }
+        curl_close($ch);
+       
+    }
+    
+}
